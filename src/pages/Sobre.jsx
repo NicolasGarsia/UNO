b3afe2e180998a32e151096be33eb522d48d355a
NICOLAS SCHNEIDER LAPOLLI GARCIA <nicolas_s_garcia@estudante.sesisenai.org.br>
2024-07-29 10:17:27 -0300

    tres

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,8 +1,34 @@
+import Nav from "../components/NavB"
+
 export default function Sobre() {
 
     return (
         <>
-            <h1>AAAAAAAAAAAAA</h1>
+            <Nav />
+
+                <div id="boxG">
+                <div>
+                    <img  id="img2" src="Uno.jpg"/>
+                </div>
+                <div id="boxT">
+                <div>
+                    <h2 id="h22"> Historia </h2>
+                </div>
+                <div>
+                    <p> A história do UNO é curiosa. O jogo é fruto de uma ‘briga’ entre um pai fã de jogos de cartas e seu filho birrento.<br></br>
+
+                        A briga aconteceu em 1971, na cidade de Reading, nos Estados Unidos. Merle Robbins e seu filho, Ray, costumavam jogar ‘Oito Maluco‘ à noite, mas conflitos sempre apareciam. Merle, em especial, criticava as regras do jogo, e queria disputar as partidas de um jeito diferente. Mas Ray batia o pé e exigia que as regras originais fossem seguidas. Confusão familiar na certa! <br></br>
+
+                        Assim, para ‘melhorar’ as regras do Oito Maluco, Merle criou um novo baralho – esse mesmo que jogamos hoje no UNO. A família aprovou as modificações e adotou o novo jogo de Merle com satisfação. Como bons norte-americanos que eram, logo os Robbins viram no divertido jogo uma oportunidade de ganhar dinheiro (quanto a isso, estavam certíssimos!). <br />
+
+                        Merle era barbeiro, portanto tinha uma clientela que poderia se transformar facilmente em possíveis compradores do novo baralho. Assim, a família economizou 8.000 dólares na época para produzir 5.000 baralhos de UNO, feitos de maneira bastante simples, e vender na barbearia. <br />
+
+                        A estratégia deu certo. Logo, o baralho colorido era mania na cidade. Não demorou muito para uma empresa maior comprar os direitos autorais do UNO, e de lá pra cá, após fusões e aquisições, o UNO se tornou uma das marcas registradas mais famosas dos Estados Unidos, com produtos licenciados e distribuídos nos quatro cantos do planeta. <br />
+
+                        Atualmente, as vendas do baralho tradicional de UNO continuam boas. Além disso, existem também centenas de variações à venda, com temáticas que variam do Snoopy à boy-band NSYNC! O jogo já ultrapassou até mesmo a barreira do mundo ‘virtual’: o app oficial é um sucesso de downloads, permitindo realizar partidas online, em tempo real, com jogadores de todo o mundo.</p>
+                </div>
+            </div>
+            </div>
         </>
     )
 
